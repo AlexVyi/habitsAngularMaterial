e4dc61c2b471e6ba1426f5b6977fd0fd8d4a79e7
AlexVyi <alexandru@epk.ro>
2017-08-07 02:00:03 +0300

    added a second color theme and more responsivness

--- js/app.js
+++ js/app.js
@@ -24,6 +24,32 @@
 
 
     })
+    app.config(function ($mdThemingProvider) {
+        $mdThemingProvider.theme('my2ndAwesomeTheme').dark()
+            .primaryPalette('blue-grey',{
+                'default': '500', // by default use shade 500 from the  palette for primary intentions
+                'hue-1': '900', // use shade 900 for the <code>md-hue-1</code> class
+                'hue-2': '700', // use shade 700 for the <code>md-hue-2</code> class
+                'hue-3': '100'
+
+            })
+            .accentPalette('yellow',{
+                'default': '500', // by default use shade 500 from the  palette for primary intentions
+                'hue-1': '900', // use shade 900 for the <code>md-hue-1</code> class
+                'hue-2': '700', // use shade 700 for the <code>md-hue-2</code> class
+                'hue-3': '100'
+            })
+            .warnPalette('deep-orange')
+            .backgroundPalette('grey',{
+                'default': '500', // by default use shade 500 from the palette for primary intentions
+                'hue-1': '900', // use shade 900 for the <code>md-hue-1</code> class
+                'hue-2': '700', // use shade 700 for the <code>md-hue-2</code> class
+                'hue-3': '100'
+
+            })
+
+
+    })
 })();
-/*here find all the modules and the theme provider*/
+/*here find all the modules and the themes provider*/
 
