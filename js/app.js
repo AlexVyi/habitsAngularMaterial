163651cb25dbff953d37da8dbafaa913d47aca69
AlexVyi <alexandru@epk.ro>
2017-08-07 23:42:09 +0300

    added material icons/going forward with container view/goals displayed and explaind on landing

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@
 
     app.config(function ($mdThemingProvider) {
         $mdThemingProvider.theme('myAwesomeTheme')
-            .primaryPalette('teal',{
-                'default': '500', // by default use shade 500 from the teal palette for primary intentions
+            .primaryPalette('pink',{
+                'default': 'A100', // by default use shade 500 from the teal palette for primary intentions
                 'hue-1': '900', // use shade 900 for the <code>md-hue-1</code> class
                 'hue-2': '700', // use shade 700 for the <code>md-hue-2</code> class
                 'hue-3': '100'
